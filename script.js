2099c522bec92a846b2bb32721495ba06570f8c6
ali cugun <alicugun123@gmail.com>
2025-02-20 18:51:09 +0300

    ModV3

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function changeText() {
     let messageElement = document.getElementById("message");
 
     if (messageElement.innerText === "Butona tiklayiniz.") {
-        messageElement.innerText = "Butona tikladiniz!";
+        messageElement.innerText = "Merhaba ben volkan konak";
     } else {
         messageElement.innerText = "Butona tiklayiniz.";
     }
