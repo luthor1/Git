f8d78b75dfc92fcd7faf5bd9e7ebaf9a580478f3
ali cugun <alicugun123@gmail.com>
2025-02-20 18:36:52 +0300

    Modifices

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function changeText() {
     let messageElement = document.getElementById("message");
 
-    if (messageElement.innerText === "Bu, basit bir HTML, CSS ve JS örneğidir.") {
+    if (messageElement.innerText === "Butona tiklayiniz.") {
         messageElement.innerText = "Butona tikladiniz!";
     } else {
-        messageElement.innerText = "Bu, basit bir HTML, CSS ve JS örneğidir.";
+        messageElement.innerText = "Butona tiklayiniz.";
     }
 }
